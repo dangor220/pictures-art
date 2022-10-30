29b0d2bb1834d1357fc2a22e07fcb8c953a641fc
Danil Gordeev <dan.gor220@yandex.ru>
2022-10-30 23:56:53 +0300

    add: smoth scroll for another links

--- src/js/modules/up.js
+++ src/js/modules/up.js
@@ -1,6 +1,7 @@
 const up = (btn) => {
 
-  const btnUp = document.querySelector(btn);
+  const btnUp = document.querySelector(btn),
+        links = document.querySelectorAll('[href^="#"]');
 
   window.addEventListener('scroll', () => {
     if (document.documentElement.scrollTop > 1650) {
@@ -10,13 +11,31 @@ const up = (btn) => {
     }
   })
 
-  btnUp.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
+
+  links.forEach(link => {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+
+      const hash = this.hash,
+            targetPosition = Math.round(document.querySelector(hash).getBoundingClientRect().top);
+      
+      window.scrollTo({
+            top: targetPosition,
+            behavior: "smooth"
+      });
+
+      history.replaceState(history.state, document.title, location.href.replace(/#.*$/g, '') + hash);
+
+    })
   })
+
+  // btnUp.addEventListener('click', (e) => {
+  //   e.preventDefault();
+  //   window.scrollTo({
+  //       top: 0,
+  //       behavior: "smooth"
+  //   });
+  // })
 
 }
 
