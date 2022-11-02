6c16f9c6a25e96ecdbda9e3e5001e71e4be45748
Danil Gordeev <dan.gor220@yandex.ru>
2022-11-02 18:06:11 +0300

    add: variation smooth scroll

--- src/js/modules/up.js
+++ src/js/modules/up.js
@@ -1,42 +1,94 @@
 const up = (btn) => {
+	const btnUp = document.querySelector(btn),
+		links = document.querySelectorAll('[href^="#"]');
 
-  const btnUp = document.querySelector(btn),
-        links = document.querySelectorAll('[href^="#"]');
+	window.addEventListener('scroll', () => {
+		if (document.documentElement.scrollTop > 1650) {
+			btnUp.style.opacity = 1;
+		} else {
+			btnUp.style.opacity = 0;
+		}
+	});
 
-  window.addEventListener('scroll', () => {
-    if (document.documentElement.scrollTop > 1650) {
-      btnUp.style.opacity = 1;
-    } else {
-      btnUp.style.opacity = 0;
-    }
-  })
+	const elem = document.documentElement,
+		body = document.body;
 
+	function calcScroll() {
+		links.forEach((link) => {
+			link.addEventListener('click', function (event) {
+  
+				let scrollTop = Math.round(elem.scrollTop || body.scrollTop);
 
-  links.forEach(link => {
-    link.addEventListener('click', function(event) {
-      event.preventDefault();
+				if (this.hash !== '') {
+					event.preventDefault();
 
-      const hash = this.hash,
-            targetPosition = Math.round(document.querySelector(hash).getBoundingClientRect().top);
-      
-      window.scrollTo({
-            top: targetPosition,
-            behavior: "smooth"
-      });
+					let hashElement = document.querySelector(this.hash),
+						hashElementTop = 0;
 
-      history.replaceState(history.state, document.title, location.href.replace(/#.*$/g, '') + hash);
+					while (hashElement.offsetParent) {
+						hashElementTop += hashElement.offsetTop;
+						hashElement = hashElement.offsetParent;
+					}
 
-    })
-  })
+					hashElementTop = Math.round(hashElementTop);
+					smoothScroll(scrollTop, hashElementTop, this.hash);
+				}
+			});
+		});
+	}
 
-  // btnUp.addEventListener('click', (e) => {
-  //   e.preventDefault();
-  //   window.scrollTo({
-  //       top: 0,
-  //       behavior: "smooth"
-  //   });
-  // })
+	function smoothScroll(from, to, hash) {
+		let timeInterval = 1,
+			prevScrollTop,
+			speed;
 
-}
+		if (from < to) {
+			speed = 40;
+		} else {
+			speed = -40;
+		}
+
+		let move = setInterval(() => {
+			let scrollTop = Math.round(elem.scrollTop || body.scrollTop);
+			if (
+				prevScrollTop === scrollTop ||
+				(to > from && scrollTop >= to) ||
+				(to < from && scrollTop <= to)
+			) {
+				clearInterval(move);
+				history.replaceState(
+					history.state,
+					document.title,
+					location.href.replace(/#.*$/g, '') + hash
+				);
+			} else {
+				body.scrollTop += speed;
+				elem.scrollTop += speed;
+				prevScrollTop = scrollTop;
+			}
+		}, timeInterval);
+	}
+
+	calcScroll();
+
+	// links.forEach(link => {
+	//   link.addEventListener('click', function(event) {
+	//     event.preventDefault();
+
+	//     const hash = this.hash,
+	//           targetPosition = Math.round(document.querySelector(hash).getBoundingClientRect().top);
+
+	//     console.log(document.querySelector(hash).offsetParent);
+
+	//     window.scrollTo({
+	//           top: targetPosition,
+	//           behavior: "smooth"
+	//     });
+
+	//     history.replaceState(history.state, document.title, location.href.replace(/#.*$/g, '') + hash);
+
+	//   })
+	// })
+};
 
 export default up;
